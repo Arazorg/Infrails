9c3753bbcff1ca3ecc3fd4481b7b34c6817bb8be
Arazorg <arazorgdev@gmail.com>
2021-10-08 11:59:30 +0700

    Stable
    Stable version after unity version change

--- Assets/Scripts/System/AdsManager.cs
+++ Assets/Scripts/System/AdsManager.cs
@@ -9,7 +9,7 @@ public class AdsManager : MonoBehaviour, IUnityAdsListener
     private const string VideoId = "video";
 #if UNITY_IOS
     private const string GameId = "4101012";
-#elif UNITY_ANDROID
+#elif UNITY_ANDROID || UNITY_EDITOR
     private const string GameId = "4101013";
 #endif
 
